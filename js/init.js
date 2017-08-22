ebe8e20786df381d45822cfff85e944b4fcfa3dd
Ben <bensass123@gmail.com>
2017-08-22 17:03:05 -0400

    cards2

--- js/init.js
+++ js/init.js
@@ -1,99 +1,141 @@
-(function($){
-  //array w days events
-  var daysEvents = [
-    
-  ];
 
+  
+  var current = moment().format('MM DD YY');
+  var allEvents = [];
+
+
+
+
+  // updates w events from selected day (default: today)
+  var getDaysEvents = (date) => {
+    $('.carousel').empty();
+    $('.carousel').removeClass('initialized');
+    //$('#date').text(current);
+    console.log(date);
+    var daysEvents = [];
+    for (i in allEvents) {
+        if (allEvents[i].date === date) {
+            console.log(allEvents[i]);
+            daysEvents.push(allEvents[i]);
+        }
+    }
+    return daysEvents;
+  }
   
 
   // creates fake objects
   var createFakes = () => {
     var arr = [];
+    var venArr = ['file:///home/ben/Documents/CODE/cltmusic/img/bars/milestone.jpeg','file:///home/ben/Documents/CODE/cltmusic/img/bars/neighborhood.png','file:///home/ben/Documents/CODE/cltmusic/img/bars/rabbithole.png']
     for (i = 0; i < 4; i++) {
       arr.push({
         event: 'Event Title - Band Names' + i,
         desc: 'Description ' + i + 'description description description description description description description description description description description description description',
-        img: 'http://lorempixel.com/300/200/nightlife/' + i,
-        link: 'http://www.google.com'
+        img:  venArr[i%3],
+        href: 'http://www.google.com'
       })
     }
     return arr;
   }
 
+
   // create test events
-  var testEvents = createFakes();
+  // var testEvents = createFakes();
 
   var createCarouselCard = (obj) => {
-    //create card
-    var $card = $('<div>', {class: 'contentDiv carousel-item card sticky-action horizontal blue-grey darken-4'});
+    var logo;
+    switch(obj.venue) {
+        case 'Snug Harbor':
+            logo = 'img/bars/snug.png';
+            break;
+        case 'The Tin Roof':
+            logo = 'img/bars/tinroof.png';
+            break;
+        case 'Evening Muse':
+            logo = 'img/bars/muse.png';
+            break;
+        case 'The Milestone Club':
+            logo = 'img/bars/milestone.jpeg';
+            break;
+    }
+    console.log('Logo', logo);
+
+    //create card 
+    var $card = $('<div>', {class: 'cardDiv carousel-item card sticky-action blue-grey darken-4'});
     
     // create img and imgDiv
     var $imgDiv = $('<div>', {class: 'card-image'});
-    var $img = $('<img>',{src: obj.img, class: 'activator'});
+    var $img = $('<img>',{src: logo, class: 'activator card-bar-logo'});
     $imgDiv.append($img);
 
     //create card-content div
     var $contentDiv = $('<div>', {class: 'card-content'});
     //title
-    var $t = $('<span>', {class: 'card-title activator white-text', text: obj.event});
-    var $i = $('<i>', {class:'material-icons right', text: 'more_vert'});
+    var $t = $('<span>', {class: 'card-title activator white-text blue-grey darken-4', text: obj.event});
+    var $i = $('<i>', {class:'material-icons right', text: 'expand_more'});
     $t.append($i);
-    $contentDiv.append($t);
+
+    //truncated desc
+    var $trunc = $('<p>', {class: 'card-desc truncate', text: obj.desc});
+    
+    $contentDiv.append($t, $trunc);
    
     // card action div for tix link
-    var $actionDiv = $('<div>', {class: 'card-action'}).append('<p><a href="#">' + obj.href + '</a></p>');
+    var $actionDiv = $('<div>', {class: 'card-action activator blue-grey darken-4'}).append('<p><a href="' + obj.href + '">' + 'Event Link' + '</a></p>');
 
     // reveal-div with description
-    var $revealDiv = $('<div>', {class: 'card-reveal'});
+    var $revealDiv = $('<div>', {class: 'card-reveal blue-grey darken-3'});
     var $t2 = $('<span>', {class: 'card-title activator white-text', text: obj.event}).append('<i class="material-icons right">close</i>');
-    var $p = $('<p>', {class: 'cardDesc', text: obj.desc});
+    var $p = $('<p>', {class: 'card-desc', text: obj.desc});
+    $revealDiv.append($t2, $p);
+
+    // append all divs to card
+    $card.append($imgDiv, $contentDiv, $actionDiv, $revealDiv);
+
+    // append card to carousel
+    $('.carousel').append($card);
   }
-
-
-  //returns a div to be added to the carousel
-  // var createCarouselObj = (obj) => {
-  //   // var d = $('<div>');
-  //   var contentDiv = $('<div>', {class: 'contentDiv carousel-item card horizontal light-blue darken-2'});
-
-  //   // add event title
-  //   var h = $('<h2>').text(obj.title);
-
-  //   // add img
-  //   var imgDiv = $('<div>', {class: 'card-image'});
-  //   var img = $('<img>',{src: obj.img, class: 'activator'});
-  //   imgDiv.append(img);
-
-  //   // add desc
-  //   var p = $('<p>', {
-  //     class: 'descCarousel',
-  //     text: obj.desc
-  //   })
-
-  //   // add link
-  //   var a2 = $('<a>', {
-  //     class: 'eventLink',
-  //     href: 'http://google.com',
-  //     text: obj.href
-  //   });
-
-  //   //append carousel
-  //   contentDiv.append(h, imgDiv, p, a2);
-  //   $('.carousel').append(contentDiv);
-  // }
 
   var populateCarousel = (arr) => {
     for (var i =0; i<arr.length; i++) {
       createCarouselCard(arr[i]);
     }
+    $('.carousel').carousel({full_width: true});
+  }
+
+  var nextDate = () => {
+    current = moment(current).add(1,'days').format('MM DD YY');
+    populateCarousel(getDaysEvents(current));
+  }
+
+  var prevDate = () => {
+      current = moment(current).subtract(1,'days').format('MM DD YY');
+      populateCarousel(getDaysEvents(current));
   }
 
 
 
   $(function(){
-    populateCarousel(testEvents);
+    //next AND PREV date button
+    $('#next').click(()=>{
+      nextDate();
+    });
+
+    $('#prev').click(()=>{
+      prevDate();
+    });
+
+    //concat all data arrays
+    allEvents = allEvents.concat(muse);
+    allEvents = allEvents.concat(snug);
+    allEvents = allEvents.concat(tinroof);
+    allEvents = allEvents.concat(milestone);
+
+    populateCarousel(getDaysEvents(current));
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $('.carousel').carousel({full_width: true});
+
+    //did this in populateCarousel function instead
+    //$('.carousel').carousel({full_width: true});
 
   }); // end of document ready
-})(jQuery); // end of jQuery name space
